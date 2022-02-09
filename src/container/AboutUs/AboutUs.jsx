d707baa2810d54f411fcc12494ec21206f12105b
kais manai <kaismanai1975@gmail.com>
2022-02-09 12:42:56 +0100

    add anmation

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { images } from '../../constants';
 import './AboutUs.css';
-
+import Fade from 'react-reveal/Fade';
+import Jump from 'react-reveal/Jump';
 
 
 
 const AboutUs = () => {
-  
+  useEffect(()=>{
+    <Fade/>;
+    <Jump/>
+  })
 
 return(
 <div className="app__aboutus app__bg flex__center section__padding" id="about">
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="G_overlay" />
     </div>
-
+    
     <div className="app__aboutus-content flex__center">
+      <Fade left duration="2500">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" target="_blank"/>
         <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
-
+     </Fade>
+      <Jump duration="2500">
       <div className="app__aboutus-content_knife flex__center">
+    
         <img id="idimage" src= "/images/ball.png" alt="about_us"/>
+     
       </div>
-
+      </Jump>
+     <Fade right duration="2500">
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
+      </Fade>
     </div>
   </div>)
   
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { SubHeading } from '../../components';
-import { images } from '../../constants';
+import Bounce from 'react-reveal/Bounce';
 import './Header.css';
 
 const Header = () => (
@@ -12,10 +12,13 @@ const Header = () => (
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
-
+  
     <div className="app__wrapper_img">
+       <Bounce duration ="2000">
       <img src="/images/jab.png" alt="header_img" />
+      </Bounce>
     </div>
+    
   </div>
 );
 
