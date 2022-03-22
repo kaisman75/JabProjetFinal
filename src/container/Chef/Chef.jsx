dde325ee702b11ae9dc7c8f7c5b5a133f55a28ab
kais manai <kaismanai1975@gmail.com>
2022-03-22 12:42:14 +0100

    jab

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,46 +1,46 @@
-import React,{useState} from 'react';
-import { SubHeading } from '../../components';
-import { images } from '../../constants';
-import Imagemodal from "../../components/Modal/ImageModal";
-import './Chef.css';
+import React, { useState } from "react";
+
+import "./Chef.css";
 
 const Chef = () => {
-  const [open, setOpen] = useState(false);
-
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
-
-
-  return(
- 
-  <div className="app_chef">
-      <div className='app__chef-content'>
-       <div className="app__chef-content-img"> 
-        <a onClick={onOpenModal}>
-          <img src='/images/img17.jpg'/>
-        </a>
-      </div>
-    
-       <div className="app__chef-content_info">
-     
-              <h1>ce que nous croyons</h1>
-
-            <div className="app__chef_info-content">
-            <p>Notre devoir et de redorer le blason de notre club et d'assurer l'encadrement des jeunes dans les meilleurs condition possibles, et  leur fournir l'ambiance favorable pour rattachement plus solide au sport et nous assister à leurs tendre la main pour garantir leurs éloignements des déviations devenues courantes dans notre région </p>
-            </div>
-       </div>
-     
-    
-  </div> 
   
-        <Imagemodal open={open}  onClose={onCloseModal} center>
-         <h1>hello</h1>
-       </Imagemodal>
+  const [show, setShow] = useState(false);
+
+  return (
+
+    <div className="app_chef">
+      <div className="app__chef-content">
+        <div className="app__chef-content_info-title">
+          <h1>ce que nous croyons</h1>
+
+          <div className="app__chef_info-content">
+            <p>
+              Notre devoir et de redorer le blason de notre club et d'assurer
+              l'encadrement des jeunes dans les meilleurs condition possibles,
+              et leur fournir l'ambiance favorable pour rattachement plus solide
+              au sport et nous assister à leurs tendre la main pour garantir
+              leurs éloignements des déviations devenues courantes dans notre
+              région{" "}
+            </p>
+          </div>
+        </div>
       
-  </div> 
+         <div className="app__chef-content-img">
+      
+          <img src="/images/img17.jpg" alt="jab"/>
        
+         </div>
+      </div>
+      
+        
+   
+     
 
-)
-  }
+     
 
-export default Chef
+    </div>
+    
+  );
+};
+
+export default Chef;
